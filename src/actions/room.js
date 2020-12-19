c1fb368c88503d68f1132faa638374551cd16772
Fraser Wright <fraser.wright@eqtr.com>
2020-12-19 11:36:16 +0000

    Update room.js

--- src/actions/room.js
+++ src/actions/room.js
@@ -50,4 +50,49 @@ module.exports = {
 
 		return addCreepToRoomSpawnQueue(room, type, memory);
 	},
+
+	assessRoomThreats: (room) => {
+		if (!room) {
+			throw new Error("Invalid parameters!");
+		}
+
+		// reset every time
+		room.memory.threats = {
+			threatPositions: [],
+			threatLevel: 0,
+			creeps: {},
+			powerCreeps: {},
+		};
+
+		// TODO make more advanced threat detection and response
+		// some sort of player segregation
+		// global threat level
+		// manage global response
+
+		const hostileCreeps = room.find(FIND_HOSTILE_CREEPS, {
+			filter: (creep) => {
+				// probably need to track creep strength (i.e. threat level)
+				const threatValue = creep.getActiveBodyparts(ATTACK) + creep.getActiveBodyparts(RANGED_ATTACK);
+				const isHostile = threatValue > 0;
+
+				if (isHostile) {
+					creep.room.memory.threats.creeps[creep.id] = creep.id;
+
+					creep.room.memory.threats.threatPositions.push(creep.pos);
+
+					creep.room.memory.threats.threatLevel += threatValue;
+				}
+
+				return isHostile;
+			},
+		});
+
+		if (hostileCreeps) {
+			room.memory.hasThreats = true;
+		}
+
+		// const hostilePowerCreeps = room.find(FIND_POWER_CREEPS, {
+		// 	filter: { structureType: STRUCTURE_EXTENSION },
+		// });
+	},
 };
